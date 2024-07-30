305b75a0d771ce802e083cb7fc47966c9125f79a
Basrah123 <elijah_weeks@stu.indianhills.edu>
2024-07-30 10:05:41 -0500

    Added Dockerfile for deployment on Render

--- obj/Debug/net7.0/BlazorServerCorr.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazorServerCorr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorServerCorr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc12894eaa944c50b37c955e7dbf4645f1856a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorServerCorr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorServerCorr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
